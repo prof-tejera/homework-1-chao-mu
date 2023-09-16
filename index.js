89677f076a445b6e4004300d9bf91a1af7ce95be
Danimal <danimal@hackpoetic.com>
2023-09-16 14:55:30 -0400

    add function to search colors

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 import fetch from 'node-fetch';
-// Recommend using node-fetch for those familiar with JS fetch
 
-const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
+const COLORS_URL = 'https://nt-cdn.s3.amazonaws.com/colors.json';
+
+/**
+ * @param str the string to search inside of
+ * @param substr the substring to check the existence of
+ * @return Boolean
+ */
+const includesStr = (str, substr) => str.toUpperCase().includes(substr.toUpperCase());
+
+/**
+ * @param color color to check if matches
+ * @param name filter for color name
+ * @param hex filter for color hex code
+ * @param compName filter for complementary color name
+ * @param compHex filter for complementary color hex code
+ * @returns Boolean
+ */
+const colorMatches = ({
+  color, name, hex, compName, compHex,
+}) => {
+  let matches = true;
+  matches &&= !name || includesStr(color.name, name);
+  matches &&= !hex || color.hex === hex;
+  matches &&= !compName || color.comp.some((compCol) => includesStr(compCol.name, compName));
+  matches &&= !compHex || color.comp.some((compCol) => compCol.hex === compHex);
+
+  return matches;
+};
 
 /**
  * @param name filter for color name
@@ -10,8 +36,19 @@ const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  * @param compHex filter for complementary color hex code
  * @returns Promise
  */
-const fetchColors = ({ name, hex, compName, compHex }) => {
-  throw Error('Not implemented');
+const fetchColors = async ({
+  name, hex, compName, compHex,
+}) => {
+  const response = await fetch(COLORS_URL);
+  const colors = await response.json();
+
+  return colors.filter((color) => colorMatches({
+    color,
+    name,
+    hex,
+    compName,
+    compHex,
+  }));
 };
 
 // Leave this here
